a2a321d1b426f339a86e79675eed74676b1e703c
Rudishh <rudishofficial@gmail.com>
2024-06-04 19:10:44 +0530

    Projects will now href to their project links

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { projects } from '@/data'
 import { PinContainer } from './ui/3d-pin'
 import { FaLocationArrow } from 'react-icons/fa'
@@ -10,40 +9,42 @@ const RecentProjects = () => {
             Projects developed by me
         </h1>
         <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-12 mt-10">
-            {projects.map(({id,title,des,img,iconLists,link})=> (
-                <div key={id} className='flex items-center justify-center w-[80vw] sm:w-[570px] h-[25rem] sm:h-[32rem] lg:min-h-[32.5rem] mb-10'>
-                   <PinContainer title={link} href={link}>
-                   <div className="relative items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-  <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
-    <img src="/bg.png" alt="bg-img" className='w-full h-full object-cover' />  
-  </div>
-  <img src={img} alt={title} className='z-30 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-full max-h-full'/>
-</div>
-                    <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
-                        {title}
-                    </h1>
-                    <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
-                        {des}
-                    </p>
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+  <div key={id} className='flex items-center justify-center w-[80vw] sm:w-[570px] h-[25rem] sm:h-[32rem] lg:min-h-[32.5rem] mb-10'>
+    <PinContainer title={link} href={link}>
+      <a href={link} target="_blank" rel="noopener noreferrer" suppressHydrationWarning>
+        <div className="relative items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10" suppressHydrationWarning>
+          <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+            <img src="/bg.png" alt="bg-img" className='w-full h-full object-cover' />  
+          </div>
+          <img src={img} alt={title} className='z-30 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-full max-h-full'/>
+        </div>
+        <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+          {title}
+        </h1>
+        <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+          {des}
+        </p>
 
-                    <div className='flex items-center justify-between mt-7 mb-3'>
-                        <div className='flex items-center'>
-                            {iconLists.map((icon,index)=> (
-                                <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center' style={{
-                                    transform:`translateX(-${5 * index * 2}px)`}}>
-                                     <img src={icon} alt={icon} className='p-2'/>
-                                     </div>
-                            ))}
-                        </div>
-                        <div className='flex justify-center items-center'>
-                         <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check live site</p>
-                         <FaLocationArrow className='ms-3' color="#CBACF9"/>
-                        </div>
-                    </div>
-
-                   </PinContainer>
-                    </div>
+        <div className='flex items-center justify-between mt-7 mb-3'>
+          <div className='flex items-center'>
+            {iconLists.map((icon, index) => (
+              <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center' style={{
+                  transform:`translateX(-${5 * index * 2}px)`}}>
+                  <img src={icon} alt={icon} className='p-2'/>
+              </div>
             ))}
+          </div>
+          <div className='flex justify-center items-center'>
+            <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check live site</p>
+            <FaLocationArrow className='ms-3' color="#CBACF9"/>
+          </div>
+        </div>
+      </a>
+    </PinContainer>
+  </div>
+))}
+
         </div>
     </div>
   )
